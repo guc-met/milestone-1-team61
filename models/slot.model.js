5b3975a1f775a148fc4edace90079ce1044bfb41
Ahmed Ibrahim <74514222+IbraKafibra@users.noreply.github.com>
2020-12-25 23:02:35 +0200

    Add files via upload

---
+++ models/slot.model.js
@@ -0,0 +1,12 @@
+const { Int32 } = require('mongodb');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var slot = new Schema({
+    course_id:{type:String,required:true,default:'empty'},
+    teacher_id:{type:String,required:true,default:'empty'},
+    day:{type:String},
+    Slotnumber:{type:Number}
+})
+module.exports = mongoose.model('S',slot);
